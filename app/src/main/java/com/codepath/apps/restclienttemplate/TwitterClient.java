77ef7b42bac5057aa31b7ef7b9829df2bdfb8320
sppatel2112 <ssp279@njit.edu>
2022-02-15 23:42:36 -0500

    Updated files to see tweets on home timeline and reload tweets

--- app/src/main/java/com/codepath/apps/restclienttemplate/TwitterClient.java
+++ app/src/main/java/com/codepath/apps/restclienttemplate/TwitterClient.java
@@ -2,6 +2,7 @@ package com.codepath.apps.restclienttemplate;
 
 import android.content.Context;
 
+import com.codepath.asynchttpclient.BuildConfig;
 import com.codepath.asynchttpclient.RequestParams;
 import com.codepath.asynchttpclient.callback.JsonHttpResponseHandler;
 import com.codepath.oauth.OAuthBaseClient;
@@ -10,22 +11,22 @@ import com.github.scribejava.apis.TwitterApi;
 import com.github.scribejava.core.builder.api.BaseApi;
 
 /*
- * 
- * This is the object responsible for communicating with a REST API. 
+ *
+ * This is the object responsible for communicating with a REST API.
  * Specify the constants below to change the API being communicated with.
- * See a full list of supported API classes: 
+ * See a full list of supported API classes:
  *   https://github.com/scribejava/scribejava/tree/master/scribejava-apis/src/main/java/com/github/scribejava/apis
  * Key and Secret are provided by the developer site for the given API i.e dev.twitter.com
  * Add methods for each relevant endpoint in the API.
- * 
+ *
  * NOTE: You may want to rename this object based on the service i.e TwitterClient or FlickrClient
- * 
+ *
  */
 public class TwitterClient extends OAuthBaseClient {
-	public static final BaseApi REST_API_INSTANCE = TwitterApi.instance(); // Change this
-	public static final String REST_URL = "https://api.twitter.com/1.1"; // Change this, base API URL
-	public static final String REST_CONSUMER_KEY = BuildConfig.CONSUMER_KEY;       // Change this inside apikey.properties
-	public static final String REST_CONSUMER_SECRET = BuildConfig.CONSUMER_SECRET; // Change this inside apikey.properties
+	public static final BaseApi REST_API_INSTANCE = TwitterApi.instance();
+	public static final String REST_URL = "https://api.twitter.com/1.1";
+	public static final String REST_CONSUMER_KEY = "4KxocRp2Wh8RZ9cy1KJEjxGVy";       // Change this inside apikey.properties
+	public static final String REST_CONSUMER_SECRET = "EeyJ4vEZN3al7c0C13bMwAY3pGc2RASrampYtvJvnX1kLDHKJf"; // Change this inside apikey.properties
 
 	// Landing page to indicate the OAuth flow worked in case Chrome for Android 25+ blocks navigation back to the app.
 	public static final String FALLBACK_URL = "https://codepath.github.io/android-rest-client-template/success.html";
@@ -50,6 +51,15 @@ public class TwitterClient extends OAuthBaseClient {
 		RequestParams params = new RequestParams();
 		params.put("count", "25");
 		params.put("since_id", 1);
+		client.get(apiUrl, params, handler);
+	}
+
+	public void getNextPageOfTweets(JsonHttpResponseHandler handler, Long maxId) {
+		String apiUrl = getApiUrl("statuses/home_timeline.json");
+		// Can specify query string params directly or through RequestParams.
+		RequestParams params = new RequestParams();
+		params.put("count", "25");
+		params.put("max_id", maxId);
 		client.get(apiUrl, params, handler);
 	}
 
